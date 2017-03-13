d79a028635c1b7485da3ca95fc66129c4822ec8c
Quitch <quitch@users.noreply.github.com>
2017-03-13 17:30:27 +0000

    Javascript insertion of difficulty level selection

---
+++ ui/mods/com.pa.quitch.gwaioverhaul/difficulty.js
@@ -0,0 +1,8 @@
+// Insert difficulty levels in form
+
+$('#game-difficulty').empty().append('<option value="0">CASUAL</option>' +
+    '<option value="1">BRONZE</option>' +
+    '<option value="2">SILVER</option>' +
+    '<option value="3">GOLD</option>' +
+    '<option value="4">PLATINUM</option>' +
+    '<option value="5">UBER</option>')
