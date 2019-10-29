08db81b6f7b4d1aa8789dc7b0beffe04cc8d67fd
Quitch <Quitch@users.noreply.github.com>
2019-10-29 13:02:51 +0000

    Revert "Underscore unused param instead"
    This reverts commit 64c78563d601a51f71f2ac6617d30e133b7c7c62.

--- ui/mods/com.pa.quitch.gwaioverhaul/colour.js
+++ ui/mods/com.pa.quitch.gwaioverhaul/colour.js
@@ -1,5 +1,5 @@
 requireGW(["shared/gw_factions"], function(GWFactions) {
-  _.forEach(model.galaxy.systems(), function(system, _star) {
+  _.forEach(model.galaxy.systems(), function(system) {
     ko.computed(function() {
       var ai = system.star.ai();
       if (!ai) return;
