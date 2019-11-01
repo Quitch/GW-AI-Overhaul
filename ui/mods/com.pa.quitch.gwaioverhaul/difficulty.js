a53229a7be7bad0b522aed9f6bace927e7cd87b2
Quitch <Quitch@users.noreply.github.com>
2019-11-01 11:07:38 +0000

    Extender before viewmodel

--- ui/mods/com.pa.quitch.gwaioverhaul/difficulty.js
+++ ui/mods/com.pa.quitch.gwaioverhaul/difficulty.js
@@ -1,3 +1,35 @@
+ko.extenders.precision = function(target, precision) {
+  //create a writable computed observable to intercept writes to our observable
+  var result = ko
+    .pureComputed({
+      read: target, //always return the original observables value
+      write: function(newValue) {
+        var current = target(),
+          roundingMultiplier = Math.pow(10, precision),
+          newValueAsNum = isNaN(newValue) ? 0 : +newValue,
+          valueToWrite =
+            Math.round(newValueAsNum * roundingMultiplier) / roundingMultiplier;
+
+        //only write if it changed
+        if (valueToWrite !== current) {
+          target(valueToWrite);
+        } else {
+          //if the rounded value is the same, but a different value was written, force a notification for the current field
+          if (newValue !== current) {
+            target.notifySubscribers(valueToWrite);
+          }
+        }
+      }
+    })
+    .extend({ notify: "always" });
+
+  //initialize with current value to make sure it is rounded appropriately
+  result(target());
+
+  //return the new computed observable
+  return result;
+};
+
 // gw_start uses ko.applyBindings(model) so we put ourselves within that variable
 model.customDifficultySettings = {
   easierStart: ko.observable(false),
@@ -66,38 +98,6 @@ model.customDifficultySettings = {
     .observable(0)
     .extend({ rateLimit: { timeout: 750 }, precision: 0 }),
   unsavedChanges: ko.observable(false)
-};
-
-ko.extenders.precision = function(target, precision) {
-  //create a writable computed observable to intercept writes to our observable
-  var result = ko
-    .pureComputed({
-      read: target, //always return the original observables value
-      write: function(newValue) {
-        var current = target(),
-          roundingMultiplier = Math.pow(10, precision),
-          newValueAsNum = isNaN(newValue) ? 0 : +newValue,
-          valueToWrite =
-            Math.round(newValueAsNum * roundingMultiplier) / roundingMultiplier;
-
-        //only write if it changed
-        if (valueToWrite !== current) {
-          target(valueToWrite);
-        } else {
-          //if the rounded value is the same, but a different value was written, force a notification for the current field
-          if (newValue !== current) {
-            target.notifySubscribers(valueToWrite);
-          }
-        }
-      }
-    })
-    .extend({ notify: "always" });
-
-  //initialize with current value to make sure it is rounded appropriately
-  result(target());
-
-  //return the new computed observable
-  return result;
 };
 
 model.customDifficultySettings.easierStart.subscribe(function() {
