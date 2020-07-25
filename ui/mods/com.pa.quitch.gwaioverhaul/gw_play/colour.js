76f3b8120ac75b0c30716ff032d2b4bf4fa5660f
Quitch <Quitch@users.noreply.github.com>
2020-07-25 14:55:17 +0100

    Handle AI faction without colour

--- ui/mods/com.pa.quitch.gwaioverhaul/gw_play/colour.js
+++ ui/mods/com.pa.quitch.gwaioverhaul/gw_play/colour.js
@@ -3,12 +3,14 @@ requireGW(["shared/gw_factions"], function (GWFactions) {
     ko.computed(function () {
       var ai = system.star.ai();
       if (!ai) return;
-      var factionIndex = ai.faction;
-      var faction = GWFactions[factionIndex];
-      // Ensures we assign faction colour, not minion colour, to each system
-      var normalizedColor = _.map(faction.color[0], function (c) {
-        return c / 255;
-      });
+      if (ai.faction != 99) {
+        var factionIndex = ai.faction;
+        var faction = GWFactions[factionIndex];
+        // Ensures we assign faction colour, not minion colour, to each system
+        var normalizedColor = _.map(faction.color[0], function (c) {
+          return c / 255;
+        });
+      } else normalizedColor = [255, 255, 255];
       system.ownerColor(normalizedColor.concat(3));
       // Dependencies. These will cause the base code that updates color to rerun, so we have to run under the same conditions, and pray we run later than that code.
       system.connected();
