cc89918723783d793d7f9b8389b0880ead526ff9
Quitch <Quitch@users.noreply.github.com>
2020-07-28 18:50:12 +0100

    Update defaullt loadout hints

---
+++ ui/main/game/galactic_war/cards/gwc_start_subcdr.js
@@ -0,0 +1,76 @@
+// !LOCNS:galactic_war
+define([
+  "module",
+  "shared/gw_common",
+  "shared/gw_factions",
+  "cards/gwc_start",
+], function (module, GW, GWFactions, GWCStart) {
+  var CARD = { id: /[^/]+$/.exec(module.id).pop() };
+
+  return {
+    visible: _.constant(false),
+    summarize: _.constant("!LOC:General Commander"),
+    icon: _.constant(
+      "coui://ui/main/game/galactic_war/shared/img/red-commander.png"
+    ),
+    describe: _.constant(
+      "!LOC:The General Commander loadout contains very limited mobile forces and only two data banks. However, the loadout comes with two Sub Commanders that accompany you into each battle."
+    ),
+    hint: function () {
+      return {
+        icon:
+          "coui://ui/main/game/galactic_war/gw_play/img/tech/gwc_commander_locked.png",
+        description: "!LOC:General Commander",
+      };
+    },
+    getContext: function (_, inventory) {
+      return {
+        faction: inventory.getTag("global", "playerFaction") || 0,
+      };
+    },
+    // system is needed else the deal hangs
+    deal: function (system, context) {
+      var minions = _.shuffle(GWFactions[context.faction].minions.slice(0));
+      return {
+        params: {
+          minions: minions.slice(0, 2),
+          allowOverflow: true,
+        },
+        chance: 0,
+      };
+    },
+    buff: function (inventory, context) {
+      if (inventory.lookupCard(CARD) === 0) {
+        // Make sure we only do the start buff/dull once
+        var buffCount = inventory.getTag("", "buffCount", 0);
+        if (!buffCount) {
+          GWCStart.buff(inventory);
+          inventory.maxCards(inventory.maxCards() - 1);
+          inventory.addUnits([
+            "/pa/units/land/vehicle_factory/vehicle_factory.json",
+            "/pa/units/land/tank_light_laser/tank_light_laser.json",
+          ]);
+          inventory.minions = _.map(context.minions);
+          var minionSpecs = _.compact(_.pluck(context.minions, "commander"));
+          inventory.addUnits(minionSpecs);
+        }
+        ++buffCount;
+        inventory.setTag("", "buffCount", buffCount);
+      } else {
+        // Don't clog up a slot.
+        inventory.maxCards(inventory.maxCards() + 1);
+        GW.bank.addStartCard(CARD);
+      }
+    },
+    dull: function (inventory) {
+      if (inventory.lookupCard(CARD) === 0) {
+        var buffCount = inventory.getTag("", "buffCount", 0);
+        if (buffCount) {
+          // Perform dulls here
+
+          inventory.setTag("", "buffCount", undefined);
+        }
+      }
+    },
+  };
+});
