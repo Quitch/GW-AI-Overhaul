00b849427259fb337f3fb2bbafd4b358c96e7a8b
Quitch <Quitch@users.noreply.github.com>
2020-08-03 17:10:49 +0100

    Clarity of debug output

--- ui/mods/com.pa.quitch.gwaioverhaul/gw_start/setup.js
+++ ui/mods/com.pa.quitch.gwaioverhaul/gw_start/setup.js
@@ -951,11 +951,11 @@ requireGW(
                 });
             }
             // prettier-ignore
-            console.log("BOSS", info.boss.name, "Faction", info.boss.faction, "Eco", info.boss.econ_rate, "Count", info.boss.bossCommanders, "Dist", maxDist)
+            console.log("BOSS:", info.boss.name, "| Faction:", info.boss.faction, "| Eco:", info.boss.econ_rate.toPrecision(2), "| Count:", info.boss.bossCommanders, "| Dist:", maxDist)
             if (info.boss.minions) {
               _.times(info.boss.minions.length, function (n) {
                 // prettier-ignore
-                console.log("Minion", info.boss.minions[n].name, "Eco", info.boss.minions[n].econ_rate, "Count", info.boss.minions[n].commanderCount)
+                console.log("\tMinion:", info.boss.minions[n].name, "| Eco:", info.boss.minions[n].econ_rate.toPrecision(2), "| Count:", info.boss.minions[n].commanderCount)
               });
             }
           }
@@ -1061,17 +1061,17 @@ requireGW(
               }
             });
             // prettier-ignore
-            console.log("WORKER", worker.ai.name, "Faction", worker.ai.faction, "Eco", worker.ai.econ_rate, "Count", worker.ai.bossCommanders, "Dist", dist)
+            console.log("WORKER:", worker.ai.name, "| Faction:", worker.ai.faction, "| Eco:", worker.ai.econ_rate.toPrecision(2), "| Count:", worker.ai.bossCommanders, "| Dist:", dist)
             if (worker.ai.minions) {
               _.times(worker.ai.minions.length, function (n) {
                 // prettier-ignore
-                console.log("Minion", worker.ai.minions[n].name, "Eco", worker.ai.minions[n].econ_rate, "Count", worker.ai.minions[n].commanderCount)
+                console.log("\tMinion:", worker.ai.minions[n].name, "| Eco:", worker.ai.minions[n].econ_rate.toPrecision(2), "| Count:", worker.ai.minions[n].commanderCount)
               });
             }
             if (worker.ai.foes) {
               _.times(worker.ai.foes.length, function (n) {
                 // prettier-ignore
-                console.log("Minion", worker.ai.foes[n].name, "Eco", worker.ai.foes[n].econ_rate, "Count", worker.ai.foes[n].commanderCount)
+                console.log("\tFoe:", worker.ai.foes[n].name, "| Eco:", worker.ai.foes[n].econ_rate.toPrecision(2), "| Count:", worker.ai.foes[n].commanderCount)
               });
             }
           });
