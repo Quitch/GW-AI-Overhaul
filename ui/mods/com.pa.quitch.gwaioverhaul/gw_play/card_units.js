990b2260bb5aef57de4193f7da71c3c42099ece1
Quitch <Quitch@users.noreply.github.com>
2020-08-19 11:36:11 +0100

    Don't WARN for minions and add slots

--- ui/mods/com.pa.quitch.gwaioverhaul/gw_play/card_units.js
+++ ui/mods/com.pa.quitch.gwaioverhaul/gw_play/card_units.js
@@ -4,6 +4,7 @@ define({
       id: "gwaio_enable_bot_aa",
       units: ["/pa/units/land/bot_aa/bot_aa.json"],
     },
+    { id: "gwc_add_card_slot", units: [] },
     {
       id: "gwc_bld_efficiency_cdr",
       units: ["/pa/units/commanders/base_commander/base_commander.json"],
@@ -825,6 +826,7 @@ define({
         "/pa/units/land/tank_nuke/tank_nuke.json",
       ],
     },
+    { id: "gwc_minon", units: [] },
     {
       id: "gwc_speed_air",
       units: [
