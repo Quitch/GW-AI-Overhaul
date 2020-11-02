9a1419e5bf925bf4ebdb64ce8e364ff4c82d0459
Quitch <Quitch@users.noreply.github.com>
2020-11-02 13:56:29 +0000

    Buff loadout description grammar

--- ui/main/game/galactic_war/cards/tgw_start_tank.js
+++ ui/main/game/galactic_war/cards/tgw_start_tank.js
@@ -13,7 +13,7 @@ define([
       "coui://ui/main/game/galactic_war/shared/img/red-commander.png"
     ),
     describe: _.constant(
-      "This Commander prefers quality over quantity and has modifed its units to that end. More health, higher damage, and more splash, but slower, and with higher build costs."
+      "This Commander prefers quality over quantity and has modifed its units to that end. More health, higher damage and more splash, but slower, and with higher build costs."
     ),
     hint: function () {
       return {
