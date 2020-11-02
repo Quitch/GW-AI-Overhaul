6c2171b0c8f25facb16351ebc79002bcb41ff1a3
Quitch <Quitch@users.noreply.github.com>
2020-11-02 16:30:05 +0000

    Nomad teleport and transport structures

--- ui/main/game/galactic_war/cards/gwaio_start_nomad.js
+++ ui/main/game/galactic_war/cards/gwaio_start_nomad.js
@@ -13,7 +13,7 @@ define([
       "coui://ui/main/game/galactic_war/shared/img/red-commander.png"
     ),
     describe: _.constant(
-      "Structures are mobile. They can't be transported or use teleporters."
+      "Non-factory and non-Titan structures are mobile. Small structures can be transported and use teleporters. Medium size structures can use teleporters."
     ),
     hint: function () {
       return {
@@ -38,37 +38,46 @@ define([
           if (inventory.getTag("global", "playerFaction") === 4)
             gwaioFactionCluster.buff(inventory);
           var mods = [];
-          var units = [
-            "/pa/units/land/air_defense_adv/air_defense_adv.json",
+          var smallStructures = [
             "/pa/units/land/air_defense/air_defense.json",
-            "/pa/units/land/anti_nuke_launcher/anti_nuke_launcher.json",
-            "/pa/units/land/artillery_long/artillery_long.json",
             "/pa/units/land/artillery_short/artillery_short.json",
-            "/pa/units/land/energy_plant_adv/energy_plant_adv.json",
             "/pa/units/land/energy_plant/energy_plant.json",
             "/pa/units/land/energy_storage/energy_storage.json",
             "/pa/units/land/land_barrier/land_barrier.json",
             "/pa/units/land/land_mine/land_mine.json",
-            "/pa/units/land/laser_defense_adv/laser_defense_adv.json",
             "/pa/units/land/laser_defense_single/laser_defense_single.json",
             "/pa/units/land/laser_defense/laser_defense.json",
-            "/pa/units/land/metal_extractor_adv/metal_extractor_adv.json",
             "/pa/units/land/metal_extractor/metal_extractor.json",
             "/pa/units/land/metal_storage/metal_storage.json",
-            "/pa/units/land/nuke_launcher/nuke_launcher.json",
-            "/pa/units/land/radar_adv/radar_adv.json",
             "/pa/units/land/radar/radar.json",
-            "/pa/units/land/tactical_missile_launcher/tactical_missile_launcher.json",
-            "/pa/units/land/titan_structure/titan_structure.json",
-            "/pa/units/orbital/deep_space_radar/deep_space_radar.json",
-            "/pa/units/orbital/defense_satellite/defense_satellite.json",
-            "/pa/units/orbital/delta_v_engine/delta_v_engine.json",
-            "/pa/units/orbital/ion_defense/ion_defense.json",
-            "/pa/units/orbital/mining_platform/mining_platform.json",
-            "/pa/units/sea/torpedo_launcher_adv/torpedo_launcher_adv.json",
             "/pa/units/sea/torpedo_launcher/torpedo_launcher.json",
           ];
-          units.forEach(function (unit) {
+          var mediiumStructures = [
+            "/pa/units/land/air_defense_adv/air_defense_adv.json",
+            "/pa/units/land/energy_plant_adv/energy_plant_adv.json",
+            "/pa/units/land/laser_defense_adv/laser_defense_adv.json",
+            "/pa/units/land/metal_extractor_adv/metal_extractor_adv.json",
+            "/pa/units/land/tactical_missile_launcher/tactical_missile_launcher.json",
+            "/pa/units/orbital/ion_defense/ion_defense.json",
+            "/pa/units/sea/torpedo_launcher_adv/torpedo_launcher_adv.json",
+          ];
+          var largeStructures = [
+            "/pa/units/land/anti_nuke_launcher/anti_nuke_launcher.json",
+            "/pa/units/land/artillery_long/artillery_long.json",
+            "/pa/units/land/nuke_launcher/nuke_launcher.json",
+            "/pa/units/land/radar_adv/radar_adv.json",
+            "/pa/units/orbital/deep_space_radar/deep_space_radar.json",
+            "/pa/units/orbital/defense_satellite/defense_satellite.json",
+            "/pa/units/orbital/mining_platform/mining_platform.json",
+          ];
+          var allStructures = smallStructures.concat(
+            mediiumStructures,
+            largeStructures
+          );
+          var teleportableStructures = smallStructures.concat(
+            mediiumStructures
+          );
+          allStructures.forEach(function (unit) {
             mods.push(
               {
                 file: unit,
@@ -99,6 +108,30 @@ define([
                 path: "command_caps",
                 op: "push",
                 value: ["ORDER_Move", "ORDER_Patrol", "ORDER_Assist"],
+              }
+            );
+          });
+          smallStructures.forEach(function (unit) {
+            mods.push({
+              file: unit,
+              path: "transportable",
+              op: "replace",
+              value: { size: 1 },
+            });
+          });
+          teleportableStructures.forEach(function (unit) {
+            mods.push(
+              {
+                file: unit,
+                path: "teleportable",
+                op: "replace",
+                value: {},
+              },
+              {
+                file: unit,
+                path: "command_caps",
+                op: "push",
+                value: ["ORDER_Use"],
               }
             );
           });
