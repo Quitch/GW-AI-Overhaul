707446585e421af261385425abd1980c3f2f1e1a
Quitch <Quitch@users.noreply.github.com>
2020-11-13 21:52:38 +0000

    Remove unnecessary braces

--- ui/mods/com.pa.quitch.gwaioverhaul/units/build.js
+++ ui/mods/com.pa.quitch.gwaioverhaul/units/build.js
@@ -2,6 +2,5 @@ var gwaioBuild = {
   "/pa/units/land/bot_aa/bot_aa.json": ["bot", 18, { row: 2, column: 6 }],
 };
 
-if (_.has(Build, "HotkeyModel.SpecIdToGridMap")) {
+if (_.has(Build, "HotkeyModel.SpecIdToGridMap"))
   _.assign(Build.HotkeyModel.SpecIdToGridMap, gwaioBuild);
-}
