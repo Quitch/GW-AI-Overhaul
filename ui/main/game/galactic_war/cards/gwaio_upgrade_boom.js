bab2dcde5c98b4c4f5b269736b17b98b06982963
Quitch <Quitch@users.noreply.github.com>
2020-12-22 10:31:13 +0000

    Move Lob upgrade to Boom

---
+++ ui/main/game/galactic_war/cards/gwaio_upgrade_boom.js
@@ -0,0 +1,50 @@
+define([
+  "coui://ui/mods/com.pa.quitch.gwaioverhaul/gw_play/card_functions.js",
+], function (gwaioFunctions) {
+  return {
+    visible: _.constant(true),
+    describe: _.constant(
+      "!LOC:Boom Upgrade Tech enables the launching of Booms in place of Dox from the Lob."
+    ),
+    summarize: _.constant("!LOC:Boom Upgrade Tech"),
+    icon: _.constant(
+      "coui://ui/main/game/galactic_war/gw_play/img/tech/gwc_bot_combat.png"
+    ),
+    audio: function () {
+      return {
+        found: "/VO/Computer/gw/board_tech_available_ammunition",
+      };
+    },
+    getContext: function (galaxy) {
+      return {
+        totalSize: galaxy.stars().length,
+      };
+    },
+    deal: function () {
+      var chance = 0;
+      if (
+        gwaioFunctions.hasUnit("/pa/units/land/bot_factory/bot_factory.json") &&
+        gwaioFunctions.hasUnit("/pa/units/land/bot_bomb/bot_bomb.json") &&
+        gwaioFunctions.hasUnit(
+          "/pa/units/land/artillery_unit_launcher/artillery_unit_launcher.json"
+        )
+      )
+        chance = 60;
+
+      return { chance: chance };
+    },
+    buff: function (inventory) {
+      var mods = [
+        {
+          file:
+            "/pa/units/land/artillery_unit_launcher/artillery_unit_launcher_ammo.json",
+          path: "spawn_unit_on_death",
+          op: "replace",
+          value: "/pa/units/land/bot_bomb/bot_bomb.json",
+        },
+      ];
+      inventory.addMods(mods);
+    },
+    dull: function () {},
+  };
+});
