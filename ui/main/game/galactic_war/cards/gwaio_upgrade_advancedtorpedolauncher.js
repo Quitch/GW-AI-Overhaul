28581a09881e683f899e491eab49cf6d5447ea3b
Quitch <Quitch@users.noreply.github.com>
2020-12-23 10:08:05 +0000

    Change icon for T2 defence upgrades

--- ui/main/game/galactic_war/cards/gwaio_upgrade_advancedtorpedolauncher.js
+++ ui/main/game/galactic_war/cards/gwaio_upgrade_advancedtorpedolauncher.js
@@ -8,7 +8,7 @@ define([
     ),
     summarize: _.constant("!LOC:Advanced Torpedo Launcher Upgrade Tech"),
     icon: _.constant(
-      "coui://ui/main/game/galactic_war/gw_play/img/tech/gwc_turret.png"
+      "coui://ui/main/game/galactic_war/gw_play/img/tech/gwc_defense.png"
     ),
     audio: function () {
       return {
