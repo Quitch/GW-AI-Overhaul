db8248096dd124545caecf43f0d0da300569ad44
Quitch <Quitch@users.noreply.github.com>
2021-01-04 21:42:52 +0000

    Fix translations of GWO panel

--- ui/mods/com.pa.quitch.gwaioverhaul/gw_play/war_info.js
+++ ui/mods/com.pa.quitch.gwaioverhaul/gw_play/war_info.js
@@ -5,12 +5,42 @@ if (!gwaioWarInfoLoaded) {
 
   try {
     if (!model.game().isTutorial()) {
+      model.gwaioDifficulty = loc(
+        model.game().galaxy().stars()[model.game().galaxy().origin()].system()
+          .gwaio.difficulty
+      );
+      model.gwaioSize = loc(
+        model.game().galaxy().stars()[model.game().galaxy().origin()].system()
+          .gwaio.galaxySize
+      );
+      if (
+        model.game().galaxy().stars()[model.game().galaxy().origin()].system()
+          .gwaio.factionScaling === true
+      )
+        model.gwaioFactionScaling = loc("!LOC:Enabled");
+      else model.gwaioFactionScaling = loc("!LOC:Disabled");
+      if (
+        model.game().galaxy().stars()[model.game().galaxy().origin()].system()
+          .gwaio.easierStart === true
+      )
+        model.gwaioEasierStart = loc("!LOC:Enabled");
+      else model.gwaioEasierStart = loc("!LOC:Disabled");
+      if (
+        model.game().galaxy().stars()[model.game().galaxy().origin()].system()
+          .gwaio.tougherCommanders === true
+      )
+        model.gwaioTougherCommanders = loc("!LOC:Enabled");
+      else model.gwaioTougherCommanders = loc("!LOC:Disabled");
+      if (model.game().hardcore() === true)
+        model.gwaioHardcore = loc("!LOC:Enabled");
+      else model.gwaioHardcore = loc("!LOC:Disabled");
+
       $("#header").append(
         loadHtml(
           "coui://ui/mods/com.pa.quitch.gwaioverhaul/gw_play/war_info.html"
         )
       );
-      locUpdateDocument();
+      locTree($("#war-detail"));
     }
   } catch (e) {
     console.error(e);
