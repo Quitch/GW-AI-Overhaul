bd80b87a5126bcf2eac81ffcefd635c5425c08f8
Quitch <Quitch@users.noreply.github.com>
2021-01-24 14:22:38 +0000

    Add Orbital Launcher Upgrade Tech

---
+++ ui/main/game/galactic_war/cards/gwaio_upgrade_orbitallauncher.js
@@ -0,0 +1,47 @@
+define([
+  "coui://ui/mods/com.pa.quitch.gwaioverhaul/gw_play/card_functions.js",
+], function (gwaioFunctions) {
+  return {
+    visible: _.constant(true),
+    describe: _.constant(
+      "!LOC:Orbital Launcher Upgrade Tech enables the building of advanced units by basic vehicle manufacturing."
+    ),
+    summarize: _.constant("!LOC:Orbital Launcher Upgrade Tech"),
+    icon: _.constant(
+      "coui://ui/main/game/galactic_war/gw_play/img/tech/gwc_orbital.png"
+    ),
+    audio: function () {
+      return {
+        found: "/VO/Computer/gw/board_tech_available_orbital",
+      };
+    },
+    getContext: function (galaxy) {
+      return {
+        totalSize: galaxy.stars().length,
+      };
+    },
+    deal: function () {
+      var chance = 0;
+      if (
+        gwaioFunctions.hasUnit(
+          "/pa/units/orbital/orbital_launcher/orbital_launcher.json"
+        )
+      )
+        chance = 70;
+
+      return { chance: chance };
+    },
+    buff: function (inventory) {
+      var mods = [
+        {
+          file: "/pa/units/orbital/orbital_launcher/orbital_launcher.json",
+          path: "buildable_types",
+          op: "replace",
+          value: "Orbital & FactoryBuild",
+        },
+      ];
+      inventory.addMods(mods);
+    },
+    dull: function () {},
+  };
+});
