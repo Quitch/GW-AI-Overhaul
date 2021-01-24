49ddeab7bc28882917686f9e4705b7b5f17dfd0a
Quitch <Quitch@users.noreply.github.com>
2021-01-24 14:13:06 +0000

    Add Air Factory Upgrade Tech

---
+++ ui/main/game/galactic_war/cards/gwaio_upgrade_airfactory.js
@@ -0,0 +1,45 @@
+define([
+  "coui://ui/mods/com.pa.quitch.gwaioverhaul/gw_play/card_functions.js",
+], function (gwaioFunctions) {
+  return {
+    visible: _.constant(true),
+    describe: _.constant(
+      "!LOC:Air Factory Upgrade Tech enables the building of advanced units by basic air manufacturing."
+    ),
+    summarize: _.constant("!LOC:Air Factory Upgrade Tech"),
+    icon: _.constant(
+      "coui://ui/main/game/galactic_war/gw_play/img/tech/gwc_combat_air.png"
+    ),
+    audio: function () {
+      return {
+        found: "/VO/Computer/gw/board_tech_available_efficiency",
+      };
+    },
+    getContext: function (galaxy) {
+      return {
+        totalSize: galaxy.stars().length,
+      };
+    },
+    deal: function () {
+      var chance = 0;
+      if (
+        gwaioFunctions.hasUnit("/pa/units/air/air_factory_adv/air_factory.json")
+      )
+        chance = 70;
+
+      return { chance: chance };
+    },
+    buff: function (inventory) {
+      var mods = [
+        {
+          file: "/pa/units/air/air_factory_adv/air_factory.json",
+          path: "buildable_types",
+          op: "replace",
+          value: "Air & Mobile & FactoryBuild",
+        },
+      ];
+      inventory.addMods(mods);
+    },
+    dull: function () {},
+  };
+});
