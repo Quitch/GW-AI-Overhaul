8f115872802c084c5a3d2c6f31ef0de8cad59e7b
Quitch <Quitch@users.noreply.github.com>
2021-01-24 14:22:29 +0000

    Add Vehicle Factory Upgrade Tech

---
+++ ui/main/game/galactic_war/cards/gwaio_upgrade_vehiclefactory.js
@@ -0,0 +1,47 @@
+define([
+  "coui://ui/mods/com.pa.quitch.gwaioverhaul/gw_play/card_functions.js",
+], function (gwaioFunctions) {
+  return {
+    visible: _.constant(true),
+    describe: _.constant(
+      "!LOC:Vehicle Factory Upgrade Tech enables the building of advanced units by basic vehicle manufacturing."
+    ),
+    summarize: _.constant("!LOC:Vehicle Factory Upgrade Tech"),
+    icon: _.constant(
+      "coui://ui/main/game/galactic_war/gw_play/img/tech/gwc_vehicle.png"
+    ),
+    audio: function () {
+      return {
+        found: "/VO/Computer/gw/board_tech_available_vehicle",
+      };
+    },
+    getContext: function (galaxy) {
+      return {
+        totalSize: galaxy.stars().length,
+      };
+    },
+    deal: function () {
+      var chance = 0;
+      if (
+        gwaioFunctions.hasUnit(
+          "/pa/units/land/vehicle_factory/vehicle_factory.json"
+        )
+      )
+        chance = 70;
+
+      return { chance: chance };
+    },
+    buff: function (inventory) {
+      var mods = [
+        {
+          file: "/pa/units/land/vehicle_factory/vehicle_factory.json",
+          path: "buildable_types",
+          op: "replace",
+          value: "Tank & Mobile & FactoryBuild",
+        },
+      ];
+      inventory.addMods(mods);
+    },
+    dull: function () {},
+  };
+});
