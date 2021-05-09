3ab7f49964d3643e75e639958d937cb0233d879e
Quitch <Quitch@users.noreply.github.com>
2021-05-09 22:20:22 +0100

    Fix naval fabber upgrade checks

--- ui/main/game/galactic_war/cards/gwaio_upgrade_advancedfabricationship.js
+++ ui/main/game/galactic_war/cards/gwaio_upgrade_advancedfabricationship.js
@@ -24,6 +24,9 @@ define([
       var chance = 0;
       if (
         gwaioFunctions.hasUnit(
+          "/pa/units/sea/naval_factory_adv/naval_factory_adv.json"
+        ) &&
+        gwaioFunctions.hasUnit(
           "/pa/units/sea/fabrication_ship_adv/fabrication_ship_adv.json"
         )
       )
--- ui/main/game/galactic_war/cards/gwaio_upgrade_fabricationship.js
+++ ui/main/game/galactic_war/cards/gwaio_upgrade_fabricationship.js
@@ -24,6 +24,9 @@ define([
       var chance = 0;
       if (
         gwaioFunctions.hasUnit(
+          "/pa/units/sea/naval_factory/naval_factory.json"
+        ) &&
+        gwaioFunctions.hasUnit(
           "/pa/units/sea/fabrication_ship/fabrication_ship.json"
         )
       )
