780b0af91c542e0e9c083b3be7dd6c1d995bf6eb
Quitch <Quitch@users.noreply.github.com>
2021-05-09 21:42:10 +0100

    Use defense icon for Anchor upgrade

--- ui/main/game/galactic_war/cards/gwaio_upgrade_anchor.js
+++ ui/main/game/galactic_war/cards/gwaio_upgrade_anchor.js
@@ -8,7 +8,7 @@ define([
     ),
     summarize: _.constant("!LOC:Anchor Upgrade Tech"),
     icon: _.constant(
-      "coui://ui/main/game/galactic_war/gw_play/img/tech/gwc_orbital_fighter.png"
+      "coui://ui/main/game/galactic_war/gw_play/img/tech/gwc_defense.png"
     ),
     audio: function () {
       return {
