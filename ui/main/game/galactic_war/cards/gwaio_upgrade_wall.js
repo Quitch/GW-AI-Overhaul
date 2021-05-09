9b040af0a9518de33d2735f8b6af5e757ce48ff8
Quitch <Quitch@users.noreply.github.com>
2021-05-09 21:49:47 +0100

    Standardise defense icons for tech level

--- ui/main/game/galactic_war/cards/gwaio_upgrade_wall.js
+++ ui/main/game/galactic_war/cards/gwaio_upgrade_wall.js
@@ -8,7 +8,7 @@ define([
     ),
     summarize: _.constant("!LOC:Wall Upgrade Tech"),
     icon: _.constant(
-      "coui://ui/main/game/galactic_war/gw_play/img/tech/gwc_defense.png"
+      "coui://ui/main/game/galactic_war/gw_play/img/tech/gwc_turret.png"
     ),
     audio: function () {
       return {
