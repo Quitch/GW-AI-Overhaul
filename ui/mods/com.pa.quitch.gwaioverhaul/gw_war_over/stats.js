6e87cae3ce7edcdc9aea4a71b1c61facb1fe6846
Quitch <Quitch@users.noreply.github.com>
2021-05-11 22:43:09 +0100

    Capture difficulty result on victory screen

--- ui/mods/com.pa.quitch.gwaioverhaul/gw_war_over/stats.js
+++ ui/mods/com.pa.quitch.gwaioverhaul/gw_war_over/stats.js
@@ -3,8 +3,41 @@ var gwaioWarOverLoadoutStatsLoaded;
 if (!gwaioWarOverLoadoutStatsLoaded) {
   gwaioWarOverLoadoutStatsLoaded = true;
 
+  // Track the highest difficulty defeated for loadout icons
   function gwaioWarOverLoadoutStats() {
     try {
+      var game = model.game();
+      if (game.gameState() === "won") {
+        var difficultyLevelAsInt = _.findIndex(
+          [
+            "!LOC:Casual",
+            "!LOC:Iron",
+            "!LOC:Bronze",
+            "!LOC:Silver",
+            "!LOC:Gold",
+            "!LOC:Platinum",
+            "!LOC:Diamond",
+            "!LOC:Uber",
+          ],
+          function (difficulty) {
+            var warDifficulty = game
+              .galaxy()
+              .stars()
+              [game.galaxy().origin()].system().gwaio.difficulty;
+            return difficulty === warDifficulty;
+          }
+        );
+        var loadout = game.inventory().cards()[0].id;
+        var highestDifficultyDefeatedWithLoadout = ko
+          .observable()
+          .extend({ local: "gwaio_victory_" + loadout });
+        if (
+          difficultyLevelAsInt > highestDifficultyDefeatedWithLoadout() ||
+          !highestDifficultyDefeatedWithLoadout()
+        ) {
+          highestDifficultyDefeatedWithLoadout(difficultyLevelAsInt);
+        }
+      }
     } catch (e) {
       console.error(e);
       console.error(JSON.stringify(e));
