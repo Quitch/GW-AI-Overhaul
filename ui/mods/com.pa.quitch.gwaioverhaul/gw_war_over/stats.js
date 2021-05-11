3abd38ab4e685283c81cf61b6e29160fab6a1013
Quitch <Quitch@users.noreply.github.com>
2021-05-11 21:16:18 +0100

    Setup insertion into gw_war_over scene

---
+++ ui/mods/com.pa.quitch.gwaioverhaul/gw_war_over/stats.js
@@ -0,0 +1,14 @@
+var gwaioWarOverLoadoutStatsLoaded;
+
+if (!gwaioWarOverLoadoutStatsLoaded) {
+  gwaioWarOverLoadoutStatsLoaded = true;
+
+  function gwaioWarOverLoadoutStats() {
+    try {
+    } catch (e) {
+      console.error(e);
+      console.error(JSON.stringify(e));
+    }
+  }
+  gwaioWarOverLoadoutStats();
+}
