9c4c55893cf25f97221bd1b98c6dc1629f87a6fd
Quitch <Quitch@users.noreply.github.com>
2021-05-17 11:55:14 +0100

    Fix displaying AI name in GWO panel

--- ui/mods/com.pa.quitch.gwaioverhaul/gw_play/gwo_panel.js
+++ ui/mods/com.pa.quitch.gwaioverhaul/gw_play/gwo_panel.js
@@ -33,12 +33,6 @@ if (!gwaioWarInfoPanelLoaded) {
           } else {
             model.gwaioTougherCommanders = loc("!LOC:Disabled");
           }
-
-          if (origin.system().gwaio.quellerAI === true) {
-            model.gwaioQuellerAI = loc("!LOC:Enabled");
-          } else {
-            model.gwaioQuellerAI = loc("!LOC:Disabled");
-          }
         } else {
           model.gwaioVersion = loc("!LOC:Unknown");
           model.gwaioDifficulty = loc("!LOC:Unknown");
@@ -47,8 +41,10 @@ if (!gwaioWarInfoPanelLoaded) {
           model.gwaioSystemScaling = loc("!LOC:Enabled");
           model.gwaioEasierStart = loc("!LOC:Unknown");
           model.gwaioTougherCommanders = loc("!LOC:Unknown");
-          model.gwaioQuellerAI = loc("!LOC:Disabled");
         }
+        if (origin.system().gwaio.ai) {
+          model.gwaioQuellerAI = origin.system().gwaio.ai;
+        } else model.gwaioQuellerAI = "Titans";
         if (model.game().hardcore() === true)
           model.gwaioHardcore = loc("!LOC:Enabled");
         else model.gwaioHardcore = loc("!LOC:Disabled");
