bdfb24fbe60df271a2425ec790aa291b25f3573c
Quitch <Quitch@users.noreply.github.com>
2021-06-01 09:22:02 +0100

    Add missing backslash to Queller ai file path

--- ui/mods/com.pa.quitch.gwaioverhaul/gw_play/referee.js
+++ ui/mods/com.pa.quitch.gwaioverhaul/gw_play/referee.js
@@ -477,7 +477,7 @@ if (!gwaioRefereeChangesLoaded) {
 
             if (quellerEnabled && model.game().inventory().minions().length > 0)
               var aiFilePath = "/pa/ai/queller/";
-            else if (quellerEnabled) aiFilePath = "/pa/ai/queller/q_uber";
+            else if (quellerEnabled) aiFilePath = "/pa/ai/queller/q_uber/";
             else aiFilePath = "/pa/ai/bugfix/";
 
             api.file.list(aiFilePath, true).then(function (files) {
