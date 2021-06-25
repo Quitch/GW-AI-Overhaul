4c2b42d6da2d2576eeb0939dd58491c6bc6dc77f
Quitch <Quitch@users.noreply.github.com>
2021-06-25 23:22:27 +0100

    Move comments to a more logical level

--- ui/mods/com.pa.quitch.gwaioverhaul/gw_play/referee.js
+++ ui/mods/com.pa.quitch.gwaioverhaul/gw_play/referee.js
@@ -780,8 +780,8 @@ if (!gwaioRefereeChangesLoaded) {
                           console.log("Assigning (All):", filePath);
                           if (!_.isEmpty(aiBuildOps))
                             addTechToAI(json, aiBuildOps);
+                          // Put "load" files where the AI expects them to be
                           if (_.startsWith(filePath, aiTechPath)) {
-                            // Put "load" files where the AI expects them to be
                             if (quellerEnabled) {
                               // We don't know if the aiPath contains q_uber
                               var quellerEnemyPath =
@@ -816,15 +816,15 @@ if (!gwaioRefereeChangesLoaded) {
                           if (!_.isEmpty(aiBuildOps))
                             addTechToAI(json, aiBuildOps);
                           if (quellerSubCommander) {
+                            // Put "load" files where Queller expects them to be
                             if (_.startsWith(filePath, aiTechPath)) {
-                              // Put "load" files where Queller expects them to be
                               filePath =
                                 quellerAllyPath +
                                 filePath.slice(aiTechPath.length);
                             }
                           } else {
+                            // TITANS Sub Commanders share an ai_path with the enemy so need a new one
                             if (_.startsWith(filePath, aiPath)) {
-                              // TITANS Sub Commanders share an ai_path with the enemy so need a new one
                               filePath =
                                 aiTechPath + filePath.slice(aiPath.length);
                             }
