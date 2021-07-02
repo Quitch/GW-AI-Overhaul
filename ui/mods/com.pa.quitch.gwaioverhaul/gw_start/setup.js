bb5130329910cca83067ebc6ab47e4f7cb072f33
Quitch <Quitch@users.noreply.github.com>
2021-07-02 20:47:12 +0100

    Version the files

--- ui/mods/com.pa.quitch.gwaioverhaul/gw_start/setup.js
+++ ui/mods/com.pa.quitch.gwaioverhaul/gw_start/setup.js
@@ -989,7 +989,7 @@ if (!gwaioSetupLoaded) {
                 .stars()
                 [game.galaxy().origin()].system();
               originSystem.gwaio = {};
-              originSystem.gwaio.version = "5.10.2";
+              originSystem.gwaio.version = "5.10.3";
               originSystem.gwaio.difficulty =
                 model.gwaioDifficultySettings.difficultyName();
               originSystem.gwaio.galaxySize = [
