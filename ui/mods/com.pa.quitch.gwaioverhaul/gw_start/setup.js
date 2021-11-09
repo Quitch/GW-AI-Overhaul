960442c11a8c7c53523da0f9e4a38afa28aaf627
Joel McGuinness <joel.mcguinness@gmail.com>
2021-11-09 21:06:49 +0000

    Renamed card variable

--- ui/mods/com.pa.quitch.gwaioverhaul/gw_start/setup.js
+++ ui/mods/com.pa.quitch.gwaioverhaul/gw_start/setup.js
@@ -369,9 +369,9 @@ if (!gwaioSetupLoaded) {
           var processedStartCards = {};
           var loadCount = allCards.length;
           var loaded = $.Deferred();
-          _.forEach(allCards, function (cardId) {
-            require(["cards/" + cardId.id], function (card) {
-              processedStartCards[cardId.id] = card;
+          _.forEach(allCards, function (card) {
+            require(["cards/" + card.id], function (cardFile) {
+              processedStartCards[card.id] = cardFile;
               if (--loadCount === 0) loaded.resolve();
             });
           });
