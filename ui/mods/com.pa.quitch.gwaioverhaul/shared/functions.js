07e46ef521e4840196e02cd09ca21bb53e98bf42
Quitch <Quitch@users.noreply.github.com>
2021-11-13 11:38:50 +0000

    Setup shared function for subcommander penchants

--- ui/mods/com.pa.quitch.gwaioverhaul/shared/functions.js
+++ ui/mods/com.pa.quitch.gwaioverhaul/shared/functions.js
@@ -71,5 +71,137 @@ define([
         return "/pa/ai_personalities/penchant/";
       else return "/pa/ai/";
     },
+    penchants: function (subCommander) {
+      var galaxy = model.game().galaxy();
+      var originSystem = galaxy.stars()[galaxy.origin()].system();
+      if (originSystem.gwaio.ai !== "Penchant") return;
+
+      var penchantTags = [
+        "Vanilla",
+        "Artillery",
+        "Fortress",
+        "AllTerrain",
+        "Assault",
+        "Boomer",
+        "Heavy",
+        "Infernodier",
+        "Raider",
+        "Meta",
+        "Sniper",
+        "Nuker",
+      ];
+      if (subCommander === true)
+        var penchantExclusions = [
+          [], // Vanilla
+          [], // Artillery
+          [], // Fortress
+          [], // AllTerrain
+          [], // Assault
+          [], // Boomer
+          ["NoPercentage"], // Heavy
+          ["NoPercentage"], // Infernodier
+          [], // Raider
+          ["NoPercentage"], // Meta
+          ["NoPercentage"], // Sniper
+          [], // Nuker
+        ];
+      else
+        penchantExclusions = [
+          [], // Vanilla
+          [], // Artillery
+          ["PenchantT1Defence", "PenchantT2Defence"], // Fortress
+          [
+            // AllTerrain
+            "PenchantT1Bot",
+            "PenchantT2Bot",
+            "PenchantT1Vehicle",
+            "PenchantT2Naval",
+          ],
+          [
+            // Assault
+            "PenchantT2Air",
+            "PenchantT1Bot",
+            "PenchantT1Vehicle",
+            "PenchantT2Vehicle",
+            "PenchantT1Naval",
+            "PenchantT2Naval",
+          ],
+          ["PenchantT1Bot", "PenchantT2Bot"], // Boomer
+          [
+            // Heavy
+            "NoPercentage",
+            "PenchantT2Air",
+            "PenchantT1Bot",
+            "PenchantT2Bot",
+            "PenchantT1Vehicle",
+            "PenchantT2Vehicle",
+            "PenchantT1Naval",
+            "PenchantT2Naval",
+          ],
+          [
+            // Infernodier
+            "NoPercentage",
+            "PenchantT1Bot",
+            "PenchantT2Bot",
+            "PenchantT1Vehicle",
+            "PenchantT2Vehicle",
+          ],
+          [
+            // Raider
+            "PenchantT2Air",
+            "PenchantT1Bot",
+            "PenchantT2Bot",
+            "PenchantT1Vehicle",
+            "PenchantT1Naval",
+            "PenchantT2Naval",
+          ],
+          [
+            // Meta
+            "NoPercentage",
+            "PenchantT2Air",
+            "PenchantT1Bot",
+            "PenchantT1Vehicle",
+            "PenchantT2Vehicle",
+            "PenchantT1Naval",
+            "PenchantT2Naval",
+          ],
+          [
+            // Sniper
+            "NoPercentage",
+            "PenchantT2Air",
+            "PenchantT1Bot",
+            "PenchantT2Bot",
+            "PenchantT1Vehicle",
+            "PenchantT2Vehicle",
+            "PenchantT1Naval",
+            "PenchantT2Naval",
+          ],
+          [], // Nuker
+        ];
+      var penchantNames = [
+        "", // Vanilla - no modifications
+        "!LOC:Artillery",
+        "!LOC:Fortress",
+        "!LOC:All-terrain",
+        "!LOC:Assault",
+        "!LOC:Boomer",
+        "!LOC:Heavy",
+        "!LOC:Infernodier",
+        "!LOC:Raider",
+        "!LOC:Meta",
+        "!LOC:Sniper",
+        "!LOC:Nuker",
+      ];
+      var penchantTag = _.sample(penchantTags);
+      var penchantIndex = _.indexOf(penchantTags, penchantTag);
+      var personalityTags = penchantTag.concat(
+        penchantExclusions[penchantIndex]
+      );
+      var penchantName = loc(penchantNames[penchantIndex]);
+      return {
+        penchants: personalityTags,
+        penchantName: penchantName,
+      };
+    },
   };
 });
