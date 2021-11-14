a91f6578abe943ceb37cfed0bc2469b087e128c7
Quitch <Quitch@users.noreply.github.com>
2021-11-14 11:23:43 +0000

    Version the files

--- ui/mods/com.pa.quitch.gwaioverhaul/gw_start/setup.js
+++ ui/mods/com.pa.quitch.gwaioverhaul/gw_start/setup.js
@@ -401,7 +401,7 @@ if (!gwaioSetupLoaded) {
 
           // gw_start.js
           model.makeGame = function () {
-            var version = "5.23.0";
+            var version = "5.24.0";
             console.log("War created using Galactic War Overhaul v" + version);
 
             model.newGame(undefined);
