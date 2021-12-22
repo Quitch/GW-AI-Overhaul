8f647f7bb66d0df1732c92d59e7ac90b99a34af2
Quitch <Quitch@users.noreply.github.com>
2021-12-22 16:01:15 +0000

    Remove units import from cards that don't use it

--- ui/main/game/galactic_war/cards/gwaio_upgrade_subcommander_duplication.js
+++ ui/main/game/galactic_war/cards/gwaio_upgrade_subcommander_duplication.js
@@ -1,38 +1,34 @@
-define(["coui://ui/mods/com.pa.quitch.gwaioverhaul/shared/units.js"], function (
-  gwaioUnits
-) {
-  return {
-    visible: _.constant(true),
-    describe: _.constant(
-      "!LOC:Sub Commander Duplication Tech adds an extra Commander to every Sub Commander's army."
-    ),
-    summarize: _.constant("!LOC:Sub Commander Duplication Tech"),
-    icon: _.constant(
-      "coui://ui/mods/com.pa.quitch.gwaioverhaul/gw_play/img/tech/gwc_commander_upgrade.png"
-    ),
-    audio: function () {
-      return {
-        found: "/VO/Computer/gw/board_tech_available_subcommander",
-      };
-    },
-    getContext: function (galaxy) {
-      return {
-        totalSize: galaxy.stars().length,
-      };
-    },
-    deal: function () {
-      var chance = 0;
-      if (model.game().inventory().minions().length > 0) {
-        chance = 30;
-      }
+define({
+  visible: _.constant(true),
+  describe: _.constant(
+    "!LOC:Sub Commander Duplication Tech adds an extra Commander to every Sub Commander's army."
+  ),
+  summarize: _.constant("!LOC:Sub Commander Duplication Tech"),
+  icon: _.constant(
+    "coui://ui/mods/com.pa.quitch.gwaioverhaul/gw_play/img/tech/gwc_commander_upgrade.png"
+  ),
+  audio: function () {
+    return {
+      found: "/VO/Computer/gw/board_tech_available_subcommander",
+    };
+  },
+  getContext: function (galaxy) {
+    return {
+      totalSize: galaxy.stars().length,
+    };
+  },
+  deal: function () {
+    var chance = 0;
+    if (model.game().inventory().minions().length > 0) {
+      chance = 30;
+    }
 
-      return { chance: chance };
-    },
-    buff: function () {
-      // performed in referee.js
-    },
-    dull: function () {
-      //empty
-    },
-  };
+    return { chance: chance };
+  },
+  buff: function () {
+    // performed in referee.js
+  },
+  dull: function () {
+    //empty
+  },
 });
