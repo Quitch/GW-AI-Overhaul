1b7f94a297c2f75998141d500bbd8cc07b43ac20
Quitch <Quitch@users.noreply.github.com>
2021-12-28 15:28:09 +0000

    Load requireGW() modules as early as possible

--- ui/mods/com.pa.quitch.gwaioverhaul/gw_play/cards.js
+++ ui/mods/com.pa.quitch.gwaioverhaul/gw_play/cards.js
@@ -8,81 +8,6 @@ if (!gwaioCardsLoaded) {
       var game = model.game();
 
       if (!game.isTutorial()) {
-        globals.CardViewModel = function (params) {
-          var self = this;
-
-          self.params = ko.observable(params);
-          self.id = ko.computed(function () {
-            var p = self.params();
-            return _.isObject(p) ? p.id : p;
-          });
-
-          self.visible = ko.observable(false);
-          self.desc = ko.observable();
-          self.locDesc = ko.computed(function () {
-            return loc(self.desc());
-          });
-          self.summary = ko.observable();
-          self.icon = ko.observable();
-          self.iconPlaceholder = ko.observable(); // Displayed when the icon is empty
-          self.audio = ko.observable();
-
-          self.isEmpty = ko.computed(function () {
-            return !self.id();
-          });
-          // Recognise loadouts introduced by mods as loadouts
-          self.isLoadout = ko.computed(function () {
-            return _.includes(self.id(), "_start_");
-          });
-
-          var loaded = $.Deferred();
-          self.card = loaded.promise();
-
-          var loadCard = function (card, data) {
-            if (_.isEmpty(card)) {
-              self.desc(
-                "!LOC:Data Bank holds one Tech. Explore systems to find new Tech."
-              );
-              self.summary("!LOC:Empty Data Bank");
-              self.icon(
-                "coui://ui/main/game/galactic_war/gw_play/img/tech/gwc_empty.png"
-              );
-              self.iconPlaceholder(undefined);
-              self.visible(true);
-            } else {
-              self.desc(card.describe && card.describe(data));
-              self.summary(card.summarize && card.summarize(data));
-              self.icon(card.icon && card.icon(data));
-              self.iconPlaceholder(
-                !self.icon() && (self.summary() || self.desc())
-              );
-              self.audio(card.audio && card.audio(data));
-              self.visible(
-                card.visible === true || !!(card.visible && card.visible(data))
-              );
-            }
-            loaded.resolve(card);
-          };
-
-          var loadToken = 0;
-          ko.computed(function () {
-            var data = self.params();
-            ++loadToken;
-            var myToken = loadToken;
-            var cardId = self.id();
-            if (cardId) {
-              requireGW(["cards/" + cardId], function (card) {
-                if (loadToken !== myToken) {
-                  return;
-                }
-                loadCard(card, data);
-              });
-            } else {
-              loadCard({}, data);
-            }
-          });
-        };
-
         // Allow player to delete tech cards whenever they want and add tooltips
         // showing units affected by the cards
         $("#hover-card").replaceWith(
@@ -169,6 +94,82 @@ if (!gwaioCardsLoaded) {
             gwaioUnitsToNames,
             gwaioAI
           ) {
+            globals.CardViewModel = function (params) {
+              var self = this;
+
+              self.params = ko.observable(params);
+              self.id = ko.computed(function () {
+                var p = self.params();
+                return _.isObject(p) ? p.id : p;
+              });
+
+              self.visible = ko.observable(false);
+              self.desc = ko.observable();
+              self.locDesc = ko.computed(function () {
+                return loc(self.desc());
+              });
+              self.summary = ko.observable();
+              self.icon = ko.observable();
+              self.iconPlaceholder = ko.observable(); // Displayed when the icon is empty
+              self.audio = ko.observable();
+
+              self.isEmpty = ko.computed(function () {
+                return !self.id();
+              });
+              // Recognise loadouts introduced by mods as loadouts
+              self.isLoadout = ko.computed(function () {
+                return _.includes(self.id(), "_start_");
+              });
+
+              var completed = $.Deferred();
+              self.card = completed.promise();
+
+              var loadCard = function (card, data) {
+                if (_.isEmpty(card)) {
+                  self.desc(
+                    "!LOC:Data Bank holds one Tech. Explore systems to find new Tech."
+                  );
+                  self.summary("!LOC:Empty Data Bank");
+                  self.icon(
+                    "coui://ui/main/game/galactic_war/gw_play/img/tech/gwc_empty.png"
+                  );
+                  self.iconPlaceholder(undefined);
+                  self.visible(true);
+                } else {
+                  self.desc(card.describe && card.describe(data));
+                  self.summary(card.summarize && card.summarize(data));
+                  self.icon(card.icon && card.icon(data));
+                  self.iconPlaceholder(
+                    !self.icon() && (self.summary() || self.desc())
+                  );
+                  self.audio(card.audio && card.audio(data));
+                  self.visible(
+                    card.visible === true ||
+                      !!(card.visible && card.visible(data))
+                  );
+                }
+                completed.resolve(card);
+              };
+
+              var loadToken = 0;
+              ko.computed(function () {
+                var data = self.params();
+                ++loadToken;
+                var myToken = loadToken;
+                var cardId = self.id();
+                if (cardId) {
+                  requireGW(["cards/" + cardId], function (card) {
+                    if (loadToken !== myToken) {
+                      return;
+                    }
+                    loadCard(card, data);
+                  });
+                } else {
+                  loadCard({}, data);
+                }
+              });
+            };
+
             var inventory = game.inventory();
             var playerFaction = 0;
 
--- ui/mods/com.pa.quitch.gwaioverhaul/gw_play/systems.js
+++ ui/mods/com.pa.quitch.gwaioverhaul/gw_play/systems.js
@@ -251,7 +251,6 @@ if (!gwaioSystemChangesLoaded) {
             });
           }
         }
-
         // Turn off the original selection icon before replacing model.selection()
         model.selection.visible(false);
 
@@ -301,50 +300,51 @@ if (!gwaioSystemChangesLoaded) {
           );
         });
 
-        requireGW(["shared/gw_factions"], function (GWFactions) {
-          _.forEach(model.galaxy.systems(), function (system) {
-            ko.computed(function () {
-              var ai = system.star.ai();
-              var normalizedColor = [];
-              if (!ai) {
-                return;
-              } else if (ai.treasurePlanet !== true) {
-                var faction = GWFactions[ai.faction];
-                // Ensures we assign faction colour, not minion colour, to each system
-                normalizedColor = _.map(faction.color[0], function (c) {
-                  return c / 255;
-                });
-              }
-              system.ownerColor(normalizedColor.concat(3));
-              // Dependencies. These will cause the base code that updates color to rerun
-              // so we have to run under the same conditions, and pray we run later than that code.
-              system.connected();
-              model.cheats.noFog();
-              system.star.hasCard();
+        requireGW(
+          ["shared/gw_common", "shared/gw_factions"],
+          function (GW, GWFactions) {
+            _.forEach(model.galaxy.systems(), function (system) {
+              ko.computed(function () {
+                var ai = system.star.ai();
+                var normalizedColor = [];
+                if (!ai) {
+                  return;
+                } else if (ai.treasurePlanet !== true) {
+                  var faction = GWFactions[ai.faction];
+                  // Ensures we assign faction colour, not minion colour, to each system
+                  normalizedColor = _.map(faction.color[0], function (c) {
+                    return c / 255;
+                  });
+                }
+                system.ownerColor(normalizedColor.concat(3));
+                // Dependencies. These will cause the base code that updates color to rerun
+                // so we have to run under the same conditions, and pray we run later than that code.
+                system.connected();
+                model.cheats.noFog();
+                system.star.hasCard();
+              });
             });
-          });
-        });
 
-        // Fix GWO v5.17.1 and earlier treasure planet bug when player had all loadouts unlocked
-        requireGW(["shared/gw_common"], function (GW) {
-          if (
-            gwaioSettings &&
-            _.isUndefined(gwaioSettings.treasurePlanetFixed)
-          ) {
-            for (var i = 0; i < galaxy.stars().length; i++) {
-              if (_.includes(galaxy.stars()[i].cardList(), undefined)) {
-                galaxy.stars()[i].cardList([]);
-                break;
+            // Fix GWO v5.17.1 and earlier treasure planet bug when player had all loadouts unlocked
+            if (
+              gwaioSettings &&
+              _.isUndefined(gwaioSettings.treasurePlanetFixed)
+            ) {
+              for (var i = 0; i < galaxy.stars().length; i++) {
+                if (_.includes(galaxy.stars()[i].cardList(), undefined)) {
+                  galaxy.stars()[i].cardList([]);
+                  break;
+                }
               }
+              gwaioSettings.treasurePlanetFixed = true;
+              game.saved(false);
+              model.driveAccessInProgress(true);
+              GW.manifest.saveGame(game).then(function () {
+                model.driveAccessInProgress(false);
+              });
             }
-            gwaioSettings.treasurePlanetFixed = true;
-            game.saved(false);
-            model.driveAccessInProgress(true);
-            GW.manifest.saveGame(game).then(function () {
-              model.driveAccessInProgress(false);
-            });
           }
-        });
+        );
       }
     } catch (e) {
       console.error(e);
