2204598e8ff8f4adc75f39b209f97d6a6f51ec42
Quitch <Quitch@users.noreply.github.com>
2022-02-08 12:14:02 +0000

    Split UI and setup elements into two files

--- ui/mods/com.pa.quitch.gwaioverhaul/gw_start/setup.js
+++ ui/mods/com.pa.quitch.gwaioverhaul/gw_start/setup.js
@@ -1,189 +1,16 @@
-var gwoSetupLoaded;
+var gwoWarLoaded;
 
-if (!gwoSetupLoaded) {
-  gwoSetupLoaded = true;
+if (!gwoWarLoaded) {
+  gwoWarLoaded = true;
 
-  function gwoSetup() {
+  function gwoWar() {
     try {
-      ko.extenders.decimals = function (target, decimals) {
-        // create a writable computed observable to intercept writes to our observable
-        var result = ko
-          .pureComputed({
-            read: target, // always return the original observables value
-            write: function (newValue) {
-              if (_.isString(newValue)) {
-                newValue = newValue.replace(",", ".");
-
-                newValue = parseFloat(newValue);
-                if (!isNaN(newValue)) {
-                  target(newValue);
-                }
-              }
-              var current = target(),
-                roundingMultiplier = Math.pow(10, decimals),
-                newValueAsNum = isNaN(newValue) ? 0 : +newValue,
-                valueToWrite =
-                  Math.round(newValueAsNum * roundingMultiplier) /
-                  roundingMultiplier;
-
-              // only write if it changed
-              if (valueToWrite !== current) {
-                target(valueToWrite);
-              } else {
-                /* if the rounded value is the same, but a different value was
-             written, force a notification for the current field */
-                if (newValue !== current) {
-                  target.notifySubscribers(valueToWrite);
-                }
-              }
-            },
-          })
-          .extend({ notify: "always" });
-
-        // initialize with current value to make sure it is rounded appropriately
-        result(target());
-
-        return result; // return the new computed observable
+      // Prevent changes to settings creating new galaxies
+      model.makeGame = function () {
+        //empty
       };
 
       model.newGameDifficultyIndex(0); // set the lowest difficulty as the default
-
-      // gw_start uses ko.applyBindings(model)
-      model.gwoDifficultySettings = {
-        factionScaling: ko.observable(true),
-        systemScaling: ko.observable(true),
-        easierStart: ko.observable(false),
-        ai: ko.observable(0).extend({ numeric: 0 }),
-        paLore: ko.observable(true).extend({ local: "gwaio_lore_enabled" }),
-        customDifficulty: ko.observable(false),
-        goForKill: ko.observable(false),
-        microType: ko.observableArray([0, 1, 2]),
-        microTypeDescription: ko.observable({
-          0: "!LOC:No",
-          1: "!LOC:Basic",
-          2: "!LOC:Advanced",
-        }),
-        microTypeChosen: ko.observable(0),
-        getmicroTypeDescription: function (value) {
-          return loc(model.gwoDifficultySettings.microTypeDescription()[value]);
-        },
-        mandatoryMinions: ko.observable(0).extend({
-          decimals: 2,
-        }),
-        minionMod: ko.observable(0).extend({
-          decimals: 2,
-        }),
-        priorityScoutMetalSpots: ko.observable(false),
-        useEasierSystemTemplate: ko.observable(false),
-        factoryBuildDelayMin: ko.observable(0).extend({
-          decimals: 0,
-        }),
-        factoryBuildDelayMax: ko.observable(0).extend({
-          decimals: 0,
-        }),
-        unableToExpandDelay: ko.observable(0).extend({
-          decimals: 0,
-        }),
-        enableCommanderDangerResponses: ko.observable(false),
-        perExpansionDelay: ko.observable(0).extend({
-          decimals: 0,
-        }),
-        personalityTags: ko.observableArray([
-          "Default",
-          "Tutorial",
-          "SlowerExpansion",
-          "PreventsWaste",
-          "queller",
-        ]),
-        personalityTagsDescription: ko.observable({
-          Default: "!LOC:Default",
-          Tutorial: "!LOC:Lobotomy",
-          SlowerExpansion: "!LOC:Slower Expansion",
-          PreventsWaste: "!LOC:Prevent Waste",
-          queller: "!LOC:Queller",
-        }),
-        personalityTagsChosen: ko.observableArray([]),
-        getpersonalityTagsDescription: function (value) {
-          return loc(
-            model.gwoDifficultySettings.personalityTagsDescription()[value]
-          );
-        },
-        econBase: ko.observable(0).extend({
-          decimals: 2,
-        }),
-        econRatePerDist: ko.observable(0).extend({
-          decimals: 2,
-        }),
-        maxBasicFabbers: ko.observable(0).extend({
-          decimals: 0,
-        }),
-        maxAdvancedFabbers: ko.observable(0).extend({
-          decimals: 0,
-        }),
-        startingLocationEvaluationRadius: ko.observable(0).extend({
-          decimals: 0,
-        }),
-        ffaChance: ko.observable(0).extend({
-          decimals: 0,
-        }),
-        bossCommanders: ko.observable(0).extend({
-          decimals: 0,
-        }),
-        landAnywhereChance: ko.observable(0).extend({
-          decimals: 0,
-        }),
-        suddenDeathChance: ko.observable(0).extend({
-          decimals: 0,
-        }),
-        bountyModeChance: ko.observable(0).extend({
-          decimals: 0,
-        }),
-        bountyModeValue: ko.observable(0).extend({
-          decimals: 2,
-        }),
-        factionTechHandicap: ko.observable(0).extend({
-          decimals: 2,
-        }),
-      };
-
-      $("#faction-select").before(
-        loadHtml(
-          "coui://ui/mods/com.pa.quitch.gwaioverhaul/gw_start/faction_tooltip.html"
-        )
-      );
-      $("#game-size").before(
-        loadHtml(
-          "coui://ui/mods/com.pa.quitch.gwaioverhaul/gw_start/size_tooltip.html"
-        )
-      );
-      var gameDifficultyLabelId = "#game-difficulty-label";
-      $(gameDifficultyLabelId).before(
-        loadHtml(
-          "coui://ui/mods/com.pa.quitch.gwaioverhaul/gw_start/ai_dropdown.html"
-        )
-      );
-      $(gameDifficultyLabelId).append(
-        loadHtml(
-          "coui://ui/mods/com.pa.quitch.gwaioverhaul/gw_start/difficulty_levels_tooltip.html"
-        )
-      );
-      var gameDifficultyId = "#game-difficulty";
-      $(gameDifficultyId).replaceWith(
-        loadHtml(
-          "coui://ui/mods/com.pa.quitch.gwaioverhaul/gw_start/difficulty_levels.html"
-        )
-      );
-      locTree($(gameDifficultyId));
-      $(gameDifficultyId).after(
-        loadHtml(
-          "coui://ui/mods/com.pa.quitch.gwaioverhaul/gw_start/difficulty_options.html"
-        )
-      );
-      locTree($("#difficulty-options"));
-      locTree($("#custom-difficulty-settings"));
-      // Because PA Inc wants to avoid escaping characters in HTML
-      model.gwoFactionScalingTooltip =
-        "!LOC:The number of enemy factions is adjusted for the galaxy's size.";
 
       // We change how we monitor model.ready() to prevent
       // Shared Systems for Galactic War breaking our new lobby
@@ -210,11 +37,6 @@ if (!gwoSetupLoaded) {
           model.gwoDifficultySettings.systemScaling(false);
         }
       });
-
-      // Prevent changes to settings creating new galaxies
-      model.makeGame = function () {
-        //empty
-      };
 
       requireGW(
         [
@@ -1045,5 +867,5 @@ if (!gwoSetupLoaded) {
       console.error(JSON.stringify(e));
     }
   }
-  gwoSetup();
+  gwoWar();
 }
---
+++ ui/mods/com.pa.quitch.gwaioverhaul/gw_start/ui.js
@@ -0,0 +1,191 @@
+var gwoUILoaded;
+
+if (!gwoUILoaded) {
+  gwoUILoaded = true;
+
+  function gwoUI() {
+    try {
+      ko.extenders.decimals = function (target, decimals) {
+        // create a writable computed observable to intercept writes to our observable
+        var result = ko
+          .pureComputed({
+            read: target, // always return the original observables value
+            write: function (newValue) {
+              if (_.isString(newValue)) {
+                newValue = newValue.replace(",", ".");
+
+                newValue = parseFloat(newValue);
+                if (!isNaN(newValue)) {
+                  target(newValue);
+                }
+              }
+              var current = target(),
+                roundingMultiplier = Math.pow(10, decimals),
+                newValueAsNum = isNaN(newValue) ? 0 : +newValue,
+                valueToWrite =
+                  Math.round(newValueAsNum * roundingMultiplier) /
+                  roundingMultiplier;
+
+              // only write if it changed
+              if (valueToWrite !== current) {
+                target(valueToWrite);
+              } else {
+                /* if the rounded value is the same, but a different value was
+             written, force a notification for the current field */
+                if (newValue !== current) {
+                  target.notifySubscribers(valueToWrite);
+                }
+              }
+            },
+          })
+          .extend({ notify: "always" });
+
+        // initialize with current value to make sure it is rounded appropriately
+        result(target());
+
+        return result; // return the new computed observable
+      };
+
+      // gw_start uses ko.applyBindings(model)
+      model.gwoDifficultySettings = {
+        factionScaling: ko.observable(true),
+        systemScaling: ko.observable(true),
+        easierStart: ko.observable(false),
+        ai: ko.observable(0).extend({ numeric: 0 }),
+        paLore: ko.observable(true).extend({ local: "gwaio_lore_enabled" }),
+        customDifficulty: ko.observable(false),
+        goForKill: ko.observable(false),
+        microType: ko.observableArray([0, 1, 2]),
+        microTypeDescription: ko.observable({
+          0: "!LOC:No",
+          1: "!LOC:Basic",
+          2: "!LOC:Advanced",
+        }),
+        microTypeChosen: ko.observable(0),
+        getmicroTypeDescription: function (value) {
+          return loc(model.gwoDifficultySettings.microTypeDescription()[value]);
+        },
+        mandatoryMinions: ko.observable(0).extend({
+          decimals: 2,
+        }),
+        minionMod: ko.observable(0).extend({
+          decimals: 2,
+        }),
+        priorityScoutMetalSpots: ko.observable(false),
+        useEasierSystemTemplate: ko.observable(false),
+        factoryBuildDelayMin: ko.observable(0).extend({
+          decimals: 0,
+        }),
+        factoryBuildDelayMax: ko.observable(0).extend({
+          decimals: 0,
+        }),
+        unableToExpandDelay: ko.observable(0).extend({
+          decimals: 0,
+        }),
+        enableCommanderDangerResponses: ko.observable(false),
+        perExpansionDelay: ko.observable(0).extend({
+          decimals: 0,
+        }),
+        personalityTags: ko.observableArray([
+          "Default",
+          "Tutorial",
+          "SlowerExpansion",
+          "PreventsWaste",
+          "queller",
+        ]),
+        personalityTagsDescription: ko.observable({
+          Default: "!LOC:Default",
+          Tutorial: "!LOC:Lobotomy",
+          SlowerExpansion: "!LOC:Slower Expansion",
+          PreventsWaste: "!LOC:Prevent Waste",
+          queller: "!LOC:Queller",
+        }),
+        personalityTagsChosen: ko.observableArray([]),
+        getpersonalityTagsDescription: function (value) {
+          return loc(
+            model.gwoDifficultySettings.personalityTagsDescription()[value]
+          );
+        },
+        econBase: ko.observable(0).extend({
+          decimals: 2,
+        }),
+        econRatePerDist: ko.observable(0).extend({
+          decimals: 2,
+        }),
+        maxBasicFabbers: ko.observable(0).extend({
+          decimals: 0,
+        }),
+        maxAdvancedFabbers: ko.observable(0).extend({
+          decimals: 0,
+        }),
+        startingLocationEvaluationRadius: ko.observable(0).extend({
+          decimals: 0,
+        }),
+        ffaChance: ko.observable(0).extend({
+          decimals: 0,
+        }),
+        bossCommanders: ko.observable(0).extend({
+          decimals: 0,
+        }),
+        landAnywhereChance: ko.observable(0).extend({
+          decimals: 0,
+        }),
+        suddenDeathChance: ko.observable(0).extend({
+          decimals: 0,
+        }),
+        bountyModeChance: ko.observable(0).extend({
+          decimals: 0,
+        }),
+        bountyModeValue: ko.observable(0).extend({
+          decimals: 2,
+        }),
+        factionTechHandicap: ko.observable(0).extend({
+          decimals: 2,
+        }),
+      };
+
+      $("#faction-select").before(
+        loadHtml(
+          "coui://ui/mods/com.pa.quitch.gwaioverhaul/gw_start/faction_tooltip.html"
+        )
+      );
+      $("#game-size").before(
+        loadHtml(
+          "coui://ui/mods/com.pa.quitch.gwaioverhaul/gw_start/size_tooltip.html"
+        )
+      );
+      var gameDifficultyLabelId = "#game-difficulty-label";
+      $(gameDifficultyLabelId).before(
+        loadHtml(
+          "coui://ui/mods/com.pa.quitch.gwaioverhaul/gw_start/ai_dropdown.html"
+        )
+      );
+      $(gameDifficultyLabelId).append(
+        loadHtml(
+          "coui://ui/mods/com.pa.quitch.gwaioverhaul/gw_start/difficulty_levels_tooltip.html"
+        )
+      );
+      var gameDifficultyId = "#game-difficulty";
+      $(gameDifficultyId).replaceWith(
+        loadHtml(
+          "coui://ui/mods/com.pa.quitch.gwaioverhaul/gw_start/difficulty_levels.html"
+        )
+      );
+      locTree($(gameDifficultyId));
+      $(gameDifficultyId).after(
+        loadHtml(
+          "coui://ui/mods/com.pa.quitch.gwaioverhaul/gw_start/difficulty_options.html"
+        )
+      );
+      locTree($("#difficulty-options"));
+      locTree($("#custom-difficulty-settings"));
+      // Because PA Inc wants to avoid escaping characters in HTML
+      model.gwoFactionScalingTooltip =
+        "!LOC:The number of enemy factions is adjusted for the galaxy's size.";
+    } catch (e) {
+      console.error(e);
+      console.error(JSON.stringify(e));
+    }
+  }
+  gwoUI();
+}
