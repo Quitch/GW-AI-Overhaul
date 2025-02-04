ab47fb0496f1a086c951bcd25439b624003c19d2
Quitch <Quitch@users.noreply.github.com>
2025-02-04 20:57:12 +0000

    Clearer function name

--- ui/mods/com.pa.quitch.gwaioverhaul/gw_play/referee_ai.js
+++ ui/mods/com.pa.quitch.gwaioverhaul/gw_play/referee_ai.js
@@ -173,7 +173,7 @@ define(["coui://ui/mods/com.pa.quitch.gwaioverhaul/shared/ai.js"], function (
     });
   };
 
-  const isClusterPresent = function (inventory, ai, subcommanders) {
+  const whoIsCluster = function (inventory, ai, subcommanders) {
     const isPlayerCluster = inventory.getTag("global", "playerFaction") === 4;
     const isEnemyCluster =
       gwoAI.isCluster(ai) ||
@@ -203,7 +203,7 @@ define(["coui://ui/mods/com.pa.quitch.gwaioverhaul/shared/ai.js"], function (
     const numberOfAllies = alliedCommanders.length;
     const aiFilePath =
       numberOfAllies > 0 ? gwoAI.getAIPath("all") : gwoAI.getAIPath("enemy");
-    const clusterPresence = isClusterPresent(inventory, ai, numberOfAllies);
+    const clusterPresence = whoIsCluster(inventory, ai, numberOfAllies);
     const aiToModify = aiToMod(inventory, guardians, clusterPresence);
 
     const deferred = $.Deferred();
