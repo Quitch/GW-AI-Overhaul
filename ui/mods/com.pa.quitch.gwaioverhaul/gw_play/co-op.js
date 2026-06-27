db2a6d27fe4f8adb8b4e22de0539e3db8d3b66bd
Quitch <Quitch@users.noreply.github.com>
2026-06-27 07:54:01 +0100

    Add a file for co-op specific gw_play changes

---
+++ ui/mods/com.pa.quitch.gwaioverhaul/gw_play/co-op.js
@@ -0,0 +1,18 @@
+var gwoCoopLoaded;
+
+function gwoCoop() {
+  const game = model.game();
+
+  if (gwoCoopLoaded || game.isTutorial()) {
+    return;
+  }
+
+  gwoCoopLoaded = true;
+
+  try {
+  } catch (e) {
+    console.error(e);
+    console.error(JSON.stringify(e));
+  }
+}
+gwoCoop();
