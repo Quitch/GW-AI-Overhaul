96ccce63e17ec2292a81cbd9c4c60284c6cea4dd
Quitch <Quitch@users.noreply.github.com>
2026-07-08 22:42:54 +0100

    Prep for release

--- ui/mods/com.pa.quitch.gwaioverhaul/gw_start/setup.js
+++ ui/mods/com.pa.quitch.gwaioverhaul/gw_start/setup.js
@@ -353,7 +353,7 @@ function gwoSetup() {
           const busyToken = {};
           model.makeGameBusy(busyToken);
 
-          const version = "5.92.0";
+          const version = "5.92.1";
           console.log("War created using Galactic War Overhaul v" + version);
 
           const game = new GW.Game();
