c6dddab79c927c12ac09a79d19e2ed3e8ca9ebe9
Quitch <Quitch@users.noreply.github.com>
2026-07-14 10:18:51 +0100

    Move GWInventory out of the global scope

--- ui/main/game/galactic_war/shared/js/gw_inventory.js
+++ ui/main/game/galactic_war/shared/js/gw_inventory.js
@@ -1,18 +1,16 @@
-// Add aiMods()
-
-var GWInventory = function () {
-  var self = this;
-  self.units = ko.observableArray();
-  // add location to store changes to AI based on cards held
-  self.aiMods = ko.observableArray();
-  self.mods = ko.observableArray();
-  self.maxCards = ko.observable(0);
-  self.cards = ko.observableArray();
-  self.minions = ko.observableArray([]);
-  self.tags = ko.observable({});
-};
-
 define(function () {
+  var GWInventory = function () {
+    var self = this;
+    self.units = ko.observableArray();
+    // GWO - add location to store changes to AI based on cards held
+    self.aiMods = ko.observableArray();
+    self.mods = ko.observableArray();
+    self.maxCards = ko.observable(0);
+    self.cards = ko.observableArray();
+    self.minions = ko.observableArray([]);
+    self.tags = ko.observable({});
+  };
+
   GWInventory.prototype = {
     load: function (config) {
       var self = this;
