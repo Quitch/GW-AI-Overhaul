b6dd63da3c8ec47662cca9423cd90d496bcb191b
Quitch <Quitch@users.noreply.github.com>
2026-07-28 08:46:26 +0100

    Raise economy tech chance
    Still lower than it used to be

--- ui/main/game/galactic_war/cards/gwc_cost_economy.js
+++ ui/main/game/galactic_war/cards/gwc_cost_economy.js
@@ -19,10 +19,10 @@ define([
     deal: function (system, context) {
       var sizes = GW.balance.numberOfSystems;
       if (gwoCard.travelledFar(system, context, sizes)) {
-        return { chance: 90 };
+        return { chance: 100 };
       }
       return {
-        chance: gwoCard.travelledModerate(system, context, sizes) ? 30 : 15,
+        chance: gwoCard.travelledModerate(system, context, sizes) ? 50 : 25,
       };
     },
     buff: function (inventory) {
